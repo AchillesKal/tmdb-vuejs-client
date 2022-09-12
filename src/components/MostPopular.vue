02b01c43e8288a8f44ef955a1af6be6b11dec640
AchillesKal <achilleas90gr@gmail.com>
2022-09-12 22:46:24 +0300

    Add padding to horizontal scrolling

--- src/components/MostPopular.vue
+++ src/components/MostPopular.vue
@@ -29,7 +29,7 @@ onMounted(() => {
       <div class="title">
       <h2>What Is Popular</h2>
       <div class="w-full flex gap-10 overflow-x-auto py-14">
-        <div class="shrink-0" v-for="(movie, index) in movies" :key="index">
+        <div class="shrink-0 first:pl-6 last:pr-6" v-for="(movie, index) in movies" :key="index">
           {{ movie.original_title }}
           <img :src="`https://image.tmdb.org/t/p/w185${movie.poster_path}`" />
         </div>
